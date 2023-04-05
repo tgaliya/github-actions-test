891a4667bc8e3eceb469c029a60360e450846f88
Tushar Galiya <tgaliya@msdgtl.com>
2023-04-05 10:52:20 +0530

    Updated yml file and Using allure reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,17 +29,17 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  // reporter: [
-  //   [
-  //     "allure-playwright",
-  //     {
-  //       detail: true,
-  //       outputFolder: "./allure-results",
-  //       suiteTitle: false,
-  //     },
-  //   ],
-  // ],
+  //reporter: 'html',
+  reporter: [
+    [
+      "allure-playwright",
+      {
+        detail: true,
+        outputFolder: "./allure-results",
+        suiteTitle: false,
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     browserName: 'chromium',
